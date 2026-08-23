f0e92124d1abd005e597b3aaba16d90723afb04d
Zaki <zakim881@gmail.com>
2026-08-23 23:40:13 +0530

    backend

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "zakisrg"
+    storage_account_name = "zakisstorage"
+    container_name        = "tfstate"
+    key                    = "terraform.tfstate"
+    use_oidc                = true
+  }
+}
