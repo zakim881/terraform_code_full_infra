bcbd19fc24b54cb1b440608ef175c90e4230e101
Zaki <zakim881@gmail.com>
2026-08-23 13:03:27 +0530

    Update backend.tf
    oidc

--- backend.tf
+++ backend.tf
@@ -4,6 +4,7 @@ terraform {
     storage_account_name = "zakisstorage"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    use_oidc = true
   }
 }
 //adding in new branch
