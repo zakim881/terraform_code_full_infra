ed9791ef80349ab4005dd523e0dc43671410ff5f
Zaki <zakim881@gmail.com>
2026-08-23 15:49:29 +0530

    l

--- rg.tf
+++ rg.tf
@@ -1,8 +1,4 @@
-resource "azurerm_resource_group" "rg" {
-for_each = var.rg_names
-name     = each.value
-location = "West Europe"
-}
+
 resource "azurerm_virtual_network" "example" {
   for_each = var.vnet
     name = each.value.name
@@ -10,7 +6,6 @@ resource "azurerm_virtual_network" "example" {
   resource_group_name = each.value.resource_group_name
   address_space       = each.value.address_space
 
-depends_on = [azurerm_resource_group.rg]
 }
 resource "azurerm_subnet" "example1" {
   for_each = var.subnet
