eadbce6b2199a9d0392c998ccb8c5bddea52f3af
Zaki <zakim881@gmail.com>
2026-08-28 18:41:40 +0530

    Remove unnecessary backend configuration lines

--- backend.tf
+++ backend.tf
@@ -1,9 +1,5 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "zakisrg"
-    storage_account_name = "zakisstorage"
-    container_name        = "tfstate"
-    key                    = "terraform.tfstate"
-    use_oidc                = true
+    use_oidc = true
   }
 }
